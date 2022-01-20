7e17fe36bbd2cab1c3d420196a93969c08d8d4b4
aksisos <81758845+aksisos@users.noreply.github.com>
2022-01-20 15:15:15 +0300

    overflow

--- макет 1.6/src/js/modal/modal.js
+++ макет 1.6/src/js/modal/modal.js
@@ -5,11 +5,13 @@ const openFeedback = document.querySelectorAll(".buttons__chat");
 const closeCall = modalCall.querySelector(".call__close-button");
 const closeFeedBack = modalFeedback.querySelector(".feedback__close-button");
 const whiteMask = document.querySelector(".white-mask");
+const body = document.querySelector("body");
 
 openCall.forEach((callButton) =>
   callButton.addEventListener("click", function () {
     modalCall.classList.add("call--show");
     whiteMask.classList.add("white-mask--show");
+    body.classList.add("body--overflow");
     whiteMask.style = "z-index: 4";
   })
 );
@@ -17,18 +19,21 @@ openCall.forEach((callButton) =>
 closeCall.addEventListener("click", function () {
   modalCall.classList.remove("call--show");
   whiteMask.classList.remove("white-mask--show");
+  body.classList.remove("body--overflow");
   whiteMask.style = "z-index: 2";
 });
 
 whiteMask.addEventListener("click", function () {
   modalCall.classList.remove("call--show");
   modalFeedback.classList.remove("feedback--show");
+  body.classList.remove("body--overflow");
   whiteMask.style = "z-index: 2";
 });
 
 closeFeedBack.addEventListener("click", function () {
   modalFeedback.classList.remove("feedback--show");
   whiteMask.classList.remove("white-mask--show");
+  body.classList.remove("body--overflow");
   whiteMask.style = "z-index: 2";
 });
 
@@ -36,6 +41,7 @@ openFeedback.forEach((feedbackButton) =>
   feedbackButton.addEventListener("click", function () {
     modalFeedback.classList.add("feedback--show");
     whiteMask.classList.add("white-mask--show");
+    body.classList.add("body--overflow");
     whiteMask.style = "z-index: 4";
   })
 );
--- макет 1.6/src/js/sidebar/sidebar.js
+++ макет 1.6/src/js/sidebar/sidebar.js
@@ -2,16 +2,21 @@ const sidebar = document.querySelector(".sidebar");
 const burgerButton = document.querySelector(".header__burger-button");
 const closeButton = sidebar.querySelector(".sidebar__close");
 const mask = document.querySelector(".white-mask");
+const body = document.querySelector("body");
 
 burgerButton.addEventListener("click", function () {
   sidebar.classList.add("sidebar--show");
   mask.classList.add("white-mask--show");
+  body.classList.add("body--overflow");
 });
 closeButton.addEventListener("click", function () {
   sidebar.classList.remove("sidebar--show");
   mask.classList.remove("white-mask--show");
+  body.classList.remove("body--overflow");
 });
 mask.addEventListener("click", function () {
   sidebar.classList.remove("sidebar--show");
   mask.classList.remove("white-mask--show");
+  body.classList.remove("body--overflow");
 });
+
